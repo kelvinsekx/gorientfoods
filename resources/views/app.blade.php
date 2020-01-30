be49e853d0c6ae184101e2cd9c7f63cd319d1284
kelvinsekx <utdkelvin@gmail.com>
2020-01-29 19:10:25 -1200

    fixes bug

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+    
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="apple-touch-icon" sizes="180x180" href="{{asset('images/apple-touch-icon.png')}}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{asset('images/favicon-32x32.png')}}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{asset('images/favicon-16x16.png')}}">
+        <link rel="manifest" href="{{asset('images/site.webmanifest')}}">
         <title>GorientFoods/welcome...</title>
         <!-- Fonts -->
         <link rel="stylesheet" href="css/slider.css">
